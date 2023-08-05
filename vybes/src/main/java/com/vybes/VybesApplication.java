6f141e26cd8794f66ddf3036c767215593132397
Linca, Paul <paul.linca@sky.uk>
2023-08-05 14:58:08 +0100

    VYBES-API | Add initial project

---
+++ vybes/src/main/java/com/vybes/VybesApplication.java
@@ -0,0 +1,12 @@
+package com.vybes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VybesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VybesApplication.class, args);
+    }
+}
