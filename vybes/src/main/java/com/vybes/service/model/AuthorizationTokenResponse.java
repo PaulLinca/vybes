5575009e16321e2f5322e3978d58754bf0f7a33c
Linca, Paul <paul.linca@sky.uk>
2023-09-17 18:02:16 +0100

    VYBES-API | Fix response model

--- vybes/src/main/java/com/vybes/service/model/AuthorizationTokenResponse.java
+++ vybes/src/main/java/com/vybes/service/model/AuthorizationTokenResponse.java
@@ -1,9 +1,12 @@
 package com.vybes.service.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AuthorizationTokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType;
 }
