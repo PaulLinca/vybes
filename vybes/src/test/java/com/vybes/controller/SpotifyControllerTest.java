19349b8f0b8da71bd85f0164aafab20065f0b462
Linca, Paul <paul.linca@sky.uk>
2024-03-31 20:15:48 +0100

    VYBES-API | Add controller test

---
+++ vybes/src/test/java/com/vybes/controller/SpotifyControllerTest.java
@@ -0,0 +1,33 @@
+package com.vybes.controller;
+
+import static org.mockito.Mockito.verify;
+
+import com.vybes.service.client.SpotifyService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SpotifyControllerTest {
+    private static final String STRING_PARAMETER = "param";
+
+    @Mock private SpotifyService spotifyService;
+
+    @InjectMocks private SpotifyController testee;
+
+    @Test
+    void test() {
+        testee.search(STRING_PARAMETER);
+        testee.getTrack(STRING_PARAMETER);
+        testee.getAlbum(STRING_PARAMETER);
+        testee.getArtist(STRING_PARAMETER);
+
+        verify(spotifyService).searchTrack(STRING_PARAMETER);
+        verify(spotifyService).getTrack(STRING_PARAMETER);
+        verify(spotifyService).getAlbum(STRING_PARAMETER);
+        verify(spotifyService).getArtist(STRING_PARAMETER);
+    }
+}
