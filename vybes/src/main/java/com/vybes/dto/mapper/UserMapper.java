a73766196370134348d344da32050b9c7dfcb72d
Paul <paultudorlinca@gmail.com>
2025-10-15 10:21:54 +0200

    VYBES-API | Fix profile pic mapping

--- vybes/src/main/java/com/vybes/dto/mapper/UserMapper.java
+++ vybes/src/main/java/com/vybes/dto/mapper/UserMapper.java
@@ -9,14 +9,5 @@ import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    @Mapping(
-            target = "profilePictureUrl",
-            source = "userId",
-            qualifiedByName = "buildProfilePictureUrl")
     UserDTO transformToDTO(VybesUser user);
-
-    @Named("buildProfilePictureUrl")
-    default String buildProfilePictureUrl(Long id) {
-        return "https://vybes-service.onrender.com/api/user/profilePicture/" + id;
-    }
 }
